cb04a68bf5af5cabdbe89e5d249dfb4f93f4158c
Ai-Nader <nadersayai@gmail.com>
2024-11-17 02:08:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 17-11-final
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Ai-Nader/17-11-final)
---
+++ lib/stripe.ts
@@ -0,0 +1,20 @@
+import Stripe from 'stripe'
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not defined')
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+  typescript: true,
+})
+
+export type StripeError = {
+  type: string
+  message: string
+}
+
+export type StripeMetadata = {
+  requestId: string
+  orderItems: string
+}
